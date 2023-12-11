696d45b61777ded770f57e625763d4d04a9dad9f
Saurav Jain <souravjain540@gmail.com>
2023-12-11 20:28:29 +0530

    first-commit

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TaskComponent } from './task/task.component';
 import { TasksComponent } from './tasks/tasks.component';
+import { CreateTaskComponent } from './create-task/create-task.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TaskComponent,
-    TasksComponent
+    TasksComponent,
+    CreateTaskComponent
   ],
   imports: [
    BrowserModule,
